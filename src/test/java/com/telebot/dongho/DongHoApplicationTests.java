d846350c6b0fbe6f1a287a46b4ebdb79e6ed4a45
Anh Cao <dev@convertdigital.com.au>
2024-05-21 15:16:48 +0700

    init project

---
+++ src/test/java/com/telebot/dongho/DongHoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telebot.dongho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongHoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
